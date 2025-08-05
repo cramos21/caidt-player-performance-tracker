59cb56dc4908bc8f30544ec761918eb42059ea6c
Cindy Matthews <cindy@cindys-mbp.lan>
2025-08-06 08:54:45 +1000

    Backup before Xcode cleanup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lovable.playertracker',
+  appName: 'caidt-player-performance-tracker',
+  webDir: 'dist'
+};
+
+export default config;
